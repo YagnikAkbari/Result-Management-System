02208627c90355371a42aeb8b8cbe182151072e7
YagnikAkbari2003 <yagnik.a@medkart.in>
2025-01-10 19:01:53 +0530

    addd api

--- controller/ecomm.js
+++ controller/ecomm.js
@@ -1,14 +1,106 @@
 const webHomePage = require("../jsons/web/home");
 const mobileHomePage = require("../jsons/mobile/home");
+const home = require("../jsons/web/home");
+const mHome = require("../jsons/mobile/home");
 exports.getEcommPages = async (req, res) => {
   console.log("Ecommm Web V2::::------");
 
   return res.status(200).json({
-    pages: [
-      { path: "/web", json: webHomePage },
-      { path: "/mobile", json: mobileHomePage },
-      // { path: "/web/store-locator" },
-      // { path: "/mobile/store-locator" },
-    ],
+    pages: {
+      "web/home": {
+        isCsr: false,
+        revalidation: 1520,
+        authentication: true,
+      },
+      "mobile/home": {
+        isCsr: false,
+        revalidation: 1520,
+        authentication: true,
+      },
+      "web/store-locator": {
+        isCsr: false,
+        revalidation: 1520,
+        authentication: true,
+      },
+      "mobile/order-medicine": {
+        isCsr: false,
+        revalidation: 1520,
+        authentication: true,
+      },
+      "web/order-medicine/dolo-650": {
+        isCsr: false,
+        revalidation: 1520,
+        authentication: true,
+      },
+      "mobile/order-medicine/dolo-650": {
+        isCsr: false,
+        revalidation: 1520,
+        authentication: true,
+      },
+      "web/order-medicine/dolo-500": {
+        isCsr: false,
+        revalidation: 1520,
+        authentication: true,
+      },
+      "mobile/order-medicine/dolo-500": {
+        isCsr: false,
+        revalidation: 1520,
+        authentication: true,
+      },
+      // "order-medicine/paracip-650": {
+      //   isCsr: false,
+      //   revalidation: 1520,
+      //   authentication: true,
+      // },
+      // "order-medicine/": {
+      //   isCsr: false,
+      //   revalidation: 1520,
+      //   authentication: true,
+      // },
+      // "order/5": {
+      //   isCsr: false,
+      //   revalidation: 1520,
+      //   authentication: true,
+      // },
+      // "categories/nutritional-supplements": {
+      //   isCsr: false,
+      //   revalidation: 1520,
+      //   authentication: true,
+      // },
+      // "categories/kidney-care": {
+      //   isCsr: false,
+      //   revalidation: 1520,
+      //   authentication: true,
+      // },
+      // // common for all device
+      // "privacy-policy": {
+      //   isCsr: false,
+      //   revalidation: 1520,
+      //   authentication: true,
+      // },
+      // "terms-and-conditions": {
+      //   isCsr: false,
+      //   revalidation: 1520,
+      //   authentication: true,
+      // },
+      // "lab-test": {
+      //   isCsr: false,
+      //   revalidation: 1520,
+      //   authentication: true,
+      // },
+    },
+  });
+};
+
+exports.getHomePage = async (req, res) => {
+  console.log("Ecommm Web home page V2::::------");
+  return res.status(200).json({
+    data: home,
+  });
+};
+exports.getMobileHomePage = async (req, res) => {
+  console.log("Ecommm Mobile home page V2::::------");
+  return res.status(200).json({
+    data: mHome,
   });
 };
--- routes/ecomm.js
+++ routes/ecomm.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const ecommController = require("../controller/ecomm");
 
 router.get("/pages", ecommController.getEcommPages);
+router.get("/web/home", ecommController.getHomePage);
+router.get("/mobile/home", ecommController.getMobileHomePage);
 
 module.exports = router;
